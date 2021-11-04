35fd9d5229b312c4aff3da5e8fb1967e366465a2
takemoto222 <omochi9797@icloud.com>
2021-11-04 21:16:14 +0900

    migrate2

---
+++ database/migrations/2021_11_04_211101_crate_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table->foreignId('user_id')->constrained();
+        //category_id
+        //condition_id
+        $table->string('name');
+        $table->string('description', 400);
+        $table->timestamp('created_at')->useCurrent()->nullable();
+        $table->timestamp('updated_at')->useCurrent()->nullable();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
